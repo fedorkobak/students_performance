cc33bc397a80a92e5e85030d00d9319f0f9515c5
fedor <kobfedsur@gmail.com>
2025-06-06 11:23:42 +0300

    convert data to parquet

--- procedure.py
+++ procedure.py
@@ -1,7 +1,24 @@
+import logging
+import pandas as pd
 from pathlib import Path
 from zipfile import ZipFile
 
-archive_path = Path()/".tmp"/"predict-student-performance-from-game-play.zip"
+logging.basicConfig(level=logging.INFO)
 
+# loading the csv takes a long time, it will be converted to the parquet
+tmp_path = Path()/".tmp"
+archive_path = tmp_path/"predict-student-performance-from-game-play.zip"
+csv_path = tmp_path/"csv_data"
+csv_path.mkdir(exist_ok=True)
+
+logging.info("Unzipping...")
 with ZipFile(file=archive_path, mode="r") as f:
-    f.extractall(".tmp")
+    f.extractall(csv_path)
+
+logging.info("Converting to parquet...")
+pd.read_csv(csv_path/"train.csv").to_parquet(tmp_path/"train.parquet")
+pd.read_csv(
+    csv_path/"train_labels.csv"
+).to_parquet(
+    tmp_path/"train_labels.parquet"
+)
