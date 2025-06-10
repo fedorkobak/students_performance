b41e06acb635efd252c4bbf3cb05e5d80f02c683
fedor <kobfedsur@gmail.com>
2025-06-10 17:21:51 +0300

    add BasicRNN model description

---
+++ src/model.py
@@ -0,0 +1,31 @@
+import torch
+import torch.nn as nn
+
+
+class BaiscRNN(nn.Module):
+    """
+    Basic implementation of the RNN model.
+    """
+    def __init__(
+        self,
+        input_size: int,
+        hidden_size: int,
+        output_size: int,
+        num_layers: int
+    ):
+        super().__init__()
+        self.rnn = nn.RNN(
+            input_size=input_size,
+            hidden_size=hidden_size,
+            batch_first=True,
+            num_layers=num_layers
+        )
+        self.linear = nn.Linear(
+            in_features=hidden_size,
+            out_features=output_size
+        )
+
+    def forward(self, X: torch.Tensor) -> torch.Tensor:
+        rnn_out = self.rnn(X)[-1][-1]
+        linear_out = self.linear(rnn_out)
+        return torch.sigmoid(linear_out)
