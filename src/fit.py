9b83b164b5a160583c826cc224c2f3300f8a3959
fedor <kobfedsur@gmail.com>
2025-06-12 19:58:11 +0300

    add epoch implementation

--- src/fit.py
+++ src/fit.py
@@ -1,25 +1,53 @@
 import os
+import tqdm
+import torch
 import dotenv
 import mlflow
 import logging
-import mlflow.pytorch
-import mlflow.sklearn
 
-from src.model import BaiscRNN
-# from src.data import train_dataset, train_loader, ordinal_encoder
+from src.model import BasicRNN
+from src.data import get_loaders
 
-dotenv.load_dotenv()
-mlflow.set_experiment(os.environ["MLFLOW_EXPERIMENT"])
-logging.basicConfig(level=logging.INFO)
 
-unit_size = train_dataset[0][0].shape[1]
-model = BaiscRNN(
-    input_size=unit_size,
-    hidden_size=unit_size*2,
-    output_size=18,
-    num_layers=1
-)
+def epoch(
+    model: torch.nn.Module,
+    optimizer: torch.optim.Optimizer,
+    data: torch.utils.data.DataLoader
+) -> list[float]:
+    """
+    Train model on the given data.
 
-with mlflow.start_run():
-    mlflow.pytorch.log_model(model)
-    mlflow.sklearn.log_model(ordinal_encoder, "ordinal_encoder")
+    Returns
+    -------
+    list[float]
+        The values of the loss function for each batch.
+    """
+    losses = []
+    model.train()
+    loss = torch.nn.BCELoss()
+
+    for X, y in tqdm.tqdm(data):
+        optimizer.zero_grad()
+        out = model(X)
+        loss_value = loss(out, y)
+        loss_value.backward()
+        losses.append(float(loss_value))
+        optimizer.step()
+    return losses
+
+
+if __name__ == "__main__":
+    dotenv.load_dotenv()
+    mlflow.set_experiment(os.environ["MLFLOW_EXPERIMENT"])
+    logging.basicConfig(level=logging.INFO)
+
+    train_loader, test_loader = get_loaders()
+    basic_rnn = BasicRNN(
+        input_size=train_loader.dataset.dataset.get_unit_shape(),
+        hidden_size=15,
+        output_size=train_loader.dataset.dataset.labels["question_id"].nunique(),
+        num_layers=1
+    )
+    # with mlflow.start_run():
+    #     mlflow.pytorch.log_model(model)
+    #     mlflow.sklearn.log_model(ordinal_encoder, "ordinal_encoder")
